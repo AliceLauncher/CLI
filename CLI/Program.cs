2bbb383f143e5a388c5bb7692a37add64f049c35
Tee <saltandtee@gmail.com>
2022-10-31 16:02:13 +0100

    selector

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
 
 internal class Program
 {
 
 
-    private static readonly string WelcomeScreen =
-    @"
+    private static void WelcomeScreen()
+    {
+
+        Console.Clear();
+        string text = @"
        _   _ _          
       /_\ | (_) ___ ___ 
      //_\\| | |/ __/ _ \
@@ -20,8 +24,17 @@ internal class Program
 ────────────────────────────────────────
     ";
 
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(text);
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+
+    }
+
+
     private static void Main(string[] args)
     {
+        bool isSuccess = true;
+
         bool isDebug = false;
         bool isUpdate = false;
 
@@ -30,7 +43,7 @@ internal class Program
         // defaults to curseforge if service is empty
         string appidModpack = string.Empty;
 
-        Console.WriteLine(WelcomeScreen);
+        WelcomeScreen();
 
         foreach (var @params in args)
         {
@@ -59,10 +72,10 @@ internal class Program
             }
         }
 
-        Console.Write("Login: ");
-        var user = Console.Read();
+        Console.Write(" Login: ");
+        var user = Console.ReadLine();
 
-        Console.Write("Password: ");
+        Console.Write(" Password: ");
 
         // https://stackoverflow.com/questions/3404421/password-masking-console-application
         var pass = string.Empty;
@@ -83,6 +96,89 @@ internal class Program
                 pass += keyInfo.KeyChar;
             }
         } while (key != ConsoleKey.Enter);
+
+        if (isSuccess)
+        {
+            DisplayMenu();
+        }
+        else
+        {
+            // strips the update args
+            Main(args.Select(x=>x.Replace("--update", "")).ToArray());
+        }
+
+    }
+    
+    private static void DisplayMenu()
+    {
+
+        ConsoleKeyInfo cKey;
+        int index = 0;
+
+        do
+        {
+            WelcomeScreen();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(" Logged in as: ");
+            Console.WriteLine("");
+
+            string[] options = { "Play", "Service", "Modpack", "Options" };
+            for (int i = 0; i < options.Length; i++)
+            {
+                if(index == i)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($" →{options[i]}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"  {options[i]}");
+                }
+            }
+
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(" ♥ Discord: discord.alicelauncher.gg");
+            Console.WriteLine(" ♥ Website: alicelauncher.gg");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine(" Version: 0.1");
+            Console.WriteLine(" - with love, Project Alice...");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Press X or ESC to exit.");
+
+            cKey = Console.ReadKey();
+
+            if(index != 0)
+            {
+                if (cKey.Key == ConsoleKey.UpArrow || cKey.Key == ConsoleKey.W)
+                    index--;
+            }
+
+            if(index != options.Length - 1)
+            {
+                if (cKey.Key == ConsoleKey.DownArrow || cKey.Key == ConsoleKey.S)
+                    index++;
+            }
+
+            if (cKey.Key == ConsoleKey.LeftArrow || cKey.Key == ConsoleKey.Backspace || cKey.Key == ConsoleKey.A)
+            {
+
+            }
+            if (cKey.Key == ConsoleKey.RightArrow || cKey.Key == ConsoleKey.Enter || cKey.Key == ConsoleKey.D)
+            {
+
+            }
+
+        } while (cKey.Key != ConsoleKey.Escape && cKey.Key != ConsoleKey.X);
+
     }
 
     // check on updates from github
