b3cf4775d37b9fff009d04a31bab09ece269ff89
Tee <saltandtee@gmail.com>
2022-11-01 00:06:42 +0100

    teasdwadwdawdadawdasd

--- AliceCLI/Helper.cs
+++ AliceCLI/Helper.cs
@@ -5,26 +5,24 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ICSharpCode.SharpZipLib;
-using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Tar;
+using SharpCompress;
+using SharpCompress.Archives.GZip;
+using SharpCompress.Archives.Tar;
 
 namespace AliceCLI
 {
     internal class Helper
     {
-        [Obsolete]
         public static void ExtractTarGz(string filename, string outputDir)
         {
-            Stream inStream = File.OpenRead(filename);
-            Stream gzipStream = new GZipInputStream(inStream);
-
-            TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
-            tarArchive.ExtractContents(outputDir);
-            tarArchive.Close();
-
-            gzipStream.Close();
-            inStream.Close();
+            using (var archive = GZipArchive.Open(filename))
+            {
+                archive.ExtractAllEntries();
+            }
+            using (var archive = TarArchive.Open(filename))
+            {
+                archive.ExtractAllEntries();
+            }
         }
         public static async Task ExtractGzip(string filename, string outputDir)
         {
