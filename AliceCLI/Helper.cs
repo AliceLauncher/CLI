31881549aea61f5fc363d0bf3e58d3b18726d57c
Tee <saltandtee@gmail.com>
2022-11-01 01:07:23 +0100

    adwwad

--- AliceCLI/Helper.cs
+++ AliceCLI/Helper.cs
@@ -15,21 +15,54 @@ namespace AliceCLI
 {
     internal class Helper
     {
+        public static void ExtractTar(string filename, string outputDir)
+        {
+            using (var stream = File.OpenRead(filename))
+                ExtractTar(stream, outputDir);
+        }
+
+        public static void ExtractTar(Stream stream, string outputDir)
+        {
+            var buffer = new byte[100];
+            while (true)
+            {
+                stream.Read(buffer, 0, 100);
+                var name = Encoding.ASCII.GetString(buffer).Trim('\0');
+                if (String.IsNullOrWhiteSpace(name))
+                    break;
+                stream.Seek(24, SeekOrigin.Current);
+                stream.Read(buffer, 0, 12);
+                var size = Convert.ToInt64(Encoding.UTF8.GetString(buffer, 0, 12).Trim('\0').Trim(), 8);
+
+                stream.Seek(376L, SeekOrigin.Current);
+
+                var output = Path.Combine(outputDir, name);
+                if (!Directory.Exists(Path.GetDirectoryName(output)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(output));
+                if (!name.Equals("./", StringComparison.InvariantCulture))
+                {
+                    using (var str = File.Open(output, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        var buf = new byte[size];
+                        stream.Read(buf, 0, buf.Length);
+                        str.Write(buf, 0, buf.Length);
+                    }
+                }
+
+                var pos = stream.Position;
+
+                var offset = 512 - (pos % 512);
+                if (offset == 512)
+                    offset = 0;
+
+                stream.Seek(offset, SeekOrigin.Current);
+            }
+        }
         public static void ExtractTarGz(string filename, string outputDir)
         {
             ExtractGzip(filename, filename + ".tar");
 
-            using (var archive = TarArchive.Open(filename + ".tar"))
-            {
-                foreach (var entry in archive.Entries)
-                {
-                    entry.WriteToDirectory(outputDir, new ExtractionOptions()
-                    {
-                        //ExtractFullPath = true,
-                        Overwrite = true
-                    });
-                }
-            }
+            ExtractTar(filename, outputDir);
 
             File.Delete(filename + ".tar");
 
