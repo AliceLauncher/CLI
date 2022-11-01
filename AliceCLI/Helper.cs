8b51c5c7a757714b1fa06b84c9d2db47f4a72637
Tee <saltandtee@gmail.com>
2022-11-01 01:02:38 +0100

    d12

--- AliceCLI/Helper.cs
+++ AliceCLI/Helper.cs
@@ -17,34 +17,30 @@ namespace AliceCLI
     {
         public static void ExtractTarGz(string filename, string outputDir)
         {
-            ExtractGzip(filename, filename);
+            ExtractGzip(filename, filename + ".tar");
 
-            using (var archive = TarArchive.Open(filename))
+            using (var archive = TarArchive.Open(filename + ".tar"))
             {
                 foreach (var entry in archive.Entries)
                 {
                     entry.WriteToDirectory(outputDir, new ExtractionOptions()
                     {
-                        ExtractFullPath = true,
+                        //ExtractFullPath = true,
                         Overwrite = true
                     });
                 }
             }
 
-            File.Delete(filename);
+            File.Delete(filename + ".tar");
 
         }
-        public static void ExtractGzip(string filename, string outputDir)
+        public static async Task ExtractGzip(string filename, string outputDir)
         {
-            using (var archive = GZipArchive.Open(filename))
+            using (var input = File.OpenRead(filename))
+            using (var output = File.OpenWrite(outputDir))
+            using (var gz = new GZipStream(input, CompressionMode.Decompress))
             {
-                foreach (var entry in archive.Entries)
-                {
-                    entry.WriteToDirectory(outputDir, new ExtractionOptions()
-                    {
-                        Overwrite = true
-                    });
-                }
+                await gz.CopyToAsync(output);
             }
         }
         public static void ExtractZip(string filename, string outputDir)
